fd55a56d89eef5b4fdb6ebdc93303258eab7e43a
Yogita Misal <55496505+yogi-25@users.noreply.github.com>
2020-05-20 23:03:24 +0530

    Update DoublyLinkedList.py

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -1,1 +1,32 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class Linkedlist:
+    def __init__(self):
+        self.head=None
+    def insert(self,newnode):
+        if self.head is None:
+            self.head=newnode
+        else:
+            lastnode=self.head
+            while True:
+                if lastnode.next is None:
+                    break
+                lastnode=lastnode.next
+            lastnode.next=newnode
+    def printList(self):
+        if self.head is None:
+            print("List is empty")
+            return
+        currentnode=self.head
+        while True:
+            if currentnode is None:
+                break
+            print(currentnode.data)
+            currentnode=currentnode.next
 
+firstnode=node("yogita")
+linkedlist=Linkedlist()
+linkedlist.insert(firstnode)
+linkedlist.printList()
