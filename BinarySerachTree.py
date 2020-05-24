e9d48b15dee017bb23b2391b23abb486714d18f7
yogi-25 <yogitamisal12@gmail.com>
2020-05-24 17:47:31 +0530

    Changed description

--- BinarySerachTree.py
+++ BinarySerachTree.py
@@ -1,3 +1,6 @@
+#You are given a pointer to the root of a binary search tree and values to be inserted into the tree.
+#Insert the values into their appropriate position
+#in the binary search tree and return the root of the updated binary tree. You just have to complete the function.
 class Node:
     def __init__(self, info):
         self.info = info  
