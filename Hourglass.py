da8844e0960d7b80d883a976d2cbad911ca36b38
yogi-25 <yogitamisal12@gmail.com>
2020-05-27 23:22:43 +0530

    array

---
+++ Hourglass.py
@@ -0,0 +1,13 @@
+#**2 4 5
+     #3
+   #2 3 5
+# import numpy as np
+import pprint
+n=3
+distance=[[[1 for k  in range(n)] for j in range(n)] for i in range(n)]
+pprint.pprint(distance)
+print(distance[1][1])
+
+
+
+
