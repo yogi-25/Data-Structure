78925ae315ee71f10a48250e49cc38d1b4355487
yogi-25 <yogitamisal12@gmail.com>
2020-05-28 15:23:28 +0530

    hourglass max sum

--- Hourglass.py
+++ Hourglass.py
@@ -1,13 +1,23 @@
-#**2 4 5
-     #3
-   #2 3 5
-# import numpy as np
-import pprint
-n=3
-distance=[[[1 for k  in range(n)] for j in range(n)] for i in range(n)]
-pprint.pprint(distance)
-print(distance[1][1])
 
+import math
+import os
+import random
+import re
+import sys
+def hourglassSum(arr):
+    #print(len(arr))
 
+            for i in range(len(arr) - 2):
+                for j in range(len(arr) - 2):
+                    sum.append(arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][
+                            j + 1] + arr[i + 2][j + 2])
 
+            print(max(sum))
 
+            #sum.append(arr[i][j]+arr[i][j+1])
+            #print(sum)
+arr = []
+for _ in range(3):
+        arr.append(list(map(int, input().split())))
+
+hourglassSum(arr)
